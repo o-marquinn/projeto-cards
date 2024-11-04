57782b3377f77e52316ef3f24316f71eb122a0c0
o-marquinn <marcos.mantovani.leite@escola.pr.gov.br>
2024-11-04 07:47:12 -0300

    Add files via upload

---
+++ Projeto-cards---final-main/perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Matemática',
+    'Como se calcula a área de um triângulo?',
+    'A área de um triângulo qualquer pode ser calculada, multiplicando a base pela sua altura e dividindo esse resultado por 2.'
+)
+
+criaCartao(
+    'Matemática',
+    'Valor de PI?',
+    'O valor é de aproximadamente 3,14159.'
+)
+
+criaCartao(
+    'Geograia',
+    'Qual a capital do Brasil?',
+    'A capital do brasil é brasília.'
+)
+
+criaCartao(
+    'Matemática',
+    'Como se calcula a área de um triângulo?',
+    'A área de um triângulo qualquer pode ser calculada, multiplicando a base pela sua altura e dividindo esse resultado por 2.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual  montanha mai alta do mundo?',
+    'É o Monte Everet.'
+)
